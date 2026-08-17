b090d5a3fc8c2a45d0835429ca3f45d29889a4d2
Marisol <pereiramarisol004@gmail.com>
2026-08-16 23:26:39 -0300

    arreglo de push

--- mobile/src/views/posts/create-post-view.tsx
+++ mobile/src/views/posts/create-post-view.tsx
@@ -2,28 +2,7 @@ import { useEffect, useMemo, useRef, useState } from 'react';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { Animated, BackHandler, ScrollView, StyleSheet, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-
-import React from 'react';
-import MapView from 'react-native-maps';
-
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-});
+import MapView, { Marker, type MapPressEvent } from 'react-native-maps';
 
 
 import {
@@ -37,7 +16,14 @@ import {
   TextInput,
   useTheme,
 } from 'react-native-paper';
-
+import {
+  AppDatePicker,
+  AppHeader,
+  AppScreen,
+  CategoryChip,
+  SegmentedControl,
+  SmartImage,
+} from '@/components';
 import { currentUserId } from '@/data/authors';
 import {
   getPostImageSource,
